b707ac3b7243c18e98a591a32f2ca7e34f19f632
F-bh <72809273+F-bh@users.noreply.github.com>
2023-12-13 22:08:04 +0100

    finished day 13

--- src/day13/day13.go
+++ src/day13/day13.go
@@ -4,16 +4,12 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strings"
 )
 
-type mirror struct {
-	startLine,
-	endLine int
-}
-
 func TaskOne() {
-	inputFile, err := os.ReadFile("/home/feyez/coding/Christmas-2023/src/day13/input")
+	inputFile, err := os.ReadFile("D:\\Projekte\\Christmas-2023\\src\\day13\\input")
 	if err != nil {
 		log.Fatalf("failed to open file input\nwith err: %v", err.Error())
 	}
@@ -25,15 +21,9 @@ func TaskOne() {
 		puzzles = append(puzzles, strings.Split(puzzle, "\n"))
 	}
 
-	var hMirrors []mirror
-	for _, puzzle := range puzzles {
-		hMirrors = append(hMirrors, findMirrors(puzzle)...)
-	}
-
 	hSum := 0
-	for _, m := range hMirrors {
-		//+1 to handle division by zero
-		hSum += ((m.endLine + 1) / (m.startLine + 1)) + m.startLine
+	for _, puzzle := range puzzles {
+		hSum += findMirror(puzzle)
 	}
 
 	//flip to vertical
@@ -54,81 +44,119 @@ func TaskOne() {
 	}
 
 	vSum := 0
-	var vMirrors []mirror
 	for _, puzzle := range puzzlesFlipped {
-		vMirrors = append(vMirrors, findMirrors(puzzle)...)
-	}
-
-	for _, m := range vMirrors {
-		//+1 to handle division by zero
-		vSum += ((m.endLine + 1) / (m.startLine + 1)) + m.startLine
+		vSum += findMirror(puzzle)
 	}
 
 	fmt.Println(hSum*100 + vSum)
 }
 
-func findMirrors(puzzle []string) []mirror {
-	var mirrors []mirror
+func TaskTwo() {
+	inputFile, err := os.ReadFile("D:\\Projekte\\Christmas-2023\\src\\day13\\input")
+	if err != nil {
+		log.Fatalf("failed to open file input\nwith err: %v", err.Error())
+	}
 
-findMirrored:
-	for lineIndex, line := range puzzle {
-		possibleMirror := mirror{startLine: lineIndex}
-		for nextIndex := lineIndex + 1; nextIndex < len(puzzle); nextIndex++ {
-			nextLine := puzzle[nextIndex]
+	inputs := strings.Split(string(inputFile), "\n\n")
+	var puzzles [][]string
 
-			if line == nextLine {
-				possibleMirror.endLine = nextIndex
-				mirrors = append(mirrors, possibleMirror)
-				continue findMirrored
+	for _, puzzle := range inputs {
+		puzzles = append(puzzles, strings.Split(puzzle, "\n"))
+	}
+
+	hSum := 0
+	for _, puzzle := range puzzles {
+		hSum += findMirrorWithSmudge(puzzle)
+	}
+
+	//flip to vertical
+	var puzzlesFlipped [][]string
+	for _, puzzle := range puzzles {
+		var lines []string
+		for x := 0; x < len(puzzle[0]); x++ {
+			lines = append(lines, "")
+		}
+
+		for _, puzzleLine := range puzzle {
+			for charIndex, char := range puzzleLine {
+				lines[charIndex] += string(char)
+			}
+		}
+
+		puzzlesFlipped = append(puzzlesFlipped, lines)
+	}
+
+	vSum := 0
+	for _, puzzle := range puzzlesFlipped {
+		vSum += findMirrorWithSmudge(puzzle)
+	}
+
+	fmt.Println(hSum*100 + vSum)
+}
+
+func findMirror(puzzle []string) int {
+	for lineIndex, _ := range puzzle {
+		if lineIndex == 0 {
+			continue
+		}
+
+		upper := func() []string {
+			var ret []string
+			for x := len(puzzle[:lineIndex]) - 1; x >= 0; x-- {
+				ret = append(ret, puzzle[:lineIndex][x])
+			}
+			return ret
+		}()
+		lower := puzzle[lineIndex:]
+
+		if len(upper) < len(lower) {
+			lower = lower[:len(upper)]
+		} else {
+			upper = upper[:len(lower)]
+		}
+
+		if slices.Equal(upper, lower) {
+			return lineIndex
+		}
+	}
+
+	return 0
+}
+
+func findMirrorWithSmudge(puzzle []string) int {
+	for lineIndex, _ := range puzzle {
+		if lineIndex == 0 {
+			continue
+		}
+
+		upper := func() []string {
+			var ret []string
+			for x := len(puzzle[:lineIndex]) - 1; x >= 0; x-- {
+				ret = append(ret, puzzle[:lineIndex][x])
+			}
+			return ret
+		}()
+		lower := puzzle[lineIndex:]
+
+		if len(upper) < len(lower) {
+			lower = lower[:len(upper)]
+		} else {
+			upper = upper[:len(lower)]
+		}
+
+		differences := 0
+		for x, line := range upper {
+			for charIndex, char := range line {
+				if char != rune(lower[x][charIndex]) {
+					differences++
+				}
 			}
 
 		}
 
+		if differences == 1 {
+			return lineIndex
+		}
 	}
-
-	//find edges of mirrors (they should already be ordered)
-	var validMirrors []mirror
-	for mirrorIndex := 0; mirrorIndex <= len(mirrors)-1; mirrorIndex++ {
-		m := mirrors[mirrorIndex]
-
-		//only a single line is mirrored
-		if len(mirrors) == 1 && m.endLine-m.startLine == 1 {
-			validMirrors = append(validMirrors, m)
-			mirrorIndex = m.startLine
-			break
-		}
-
-		//first two lines are mirrored
-		if mirrorIndex == 0 && m.endLine-m.startLine == 1 {
-			validMirrors = append(validMirrors, m)
-			continue
-		}
-
-		//last two lines are mirrored
-		if mirrorIndex == len(mirrors)-2 && m.endLine-m.startLine == 1 {
-			validMirrors = append(validMirrors, m)
-			break
-		}
-
-		//all lines up to the end of this mirror are also mirrored inside it's start and end
-		containedMirrors := 0
-		for _, m2 := range mirrors[mirrorIndex+1:] {
-			if m2.startLine > m.startLine && m2.endLine < m.endLine {
-				containedMirrors++
-				continue
-			}
-
-			break
-		}
-
-		if containedMirrors*2 == m.endLine-m.startLine-1 {
-			validMirrors = append(validMirrors, m)
-			mirrorIndex += containedMirrors
-			continue
-		}
-
-		mirrorIndex++
-	}
-
-	return validMirrors
+	return 0
 }
